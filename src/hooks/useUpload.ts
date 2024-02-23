e09793180eca72010a403050786ca04d03521417
VoidCoder <3049988597@qq.com>
2024-02-23 22:14:53 +0800

    imgUrl

--- src/hooks/useUpload.ts
+++ src/hooks/useUpload.ts
@@ -1,18 +1,18 @@
-
 import { create } from "zustand";
 
 interface IUseUpload {
-  imgUrl: string,
-  setImg: (url: string) => void
+  imgUrl: string;
+  setImg: (url: string) => void;
 }
 
 const useUpload = create<IUseUpload>((set) => ({
-  imgUrl: '/src/images/zi.jpg',
+  imgUrl:
+    "https://res.cloudinary.com/dtzi3qstg/image/upload/v1708697623/ubsjspf93uvxima0ln7e.jpg",
   setImg: (url) => {
     set({
-      imgUrl: url
-    })
-  }
-}))
+      imgUrl: url,
+    });
+  },
+}));
 
 export { useUpload };
